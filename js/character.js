c0585598998b8c82c697d8980d59edf6ed8a6c86
Tenebrius <etignis@gmail.com>
2017-11-09 12:32:05 +0300

    feat[data]: character model

---
+++ js/character.js
@@ -0,0 +1,30 @@
+var oCharacter = {
+  name: "",
+  race: "",
+  subrace: "",
+  XP: 0,
+  aClass: [
+    {
+      name: "fighter",
+      level: 5,
+      archetype: "",
+    }
+  ],
+  oAbilities: {
+    strength: 10,
+    dexterity: 10,
+    constitution: 10,
+    intelligence: 10,
+    wisdom: 10,
+    charisma: 10
+  },
+  aArmor: [
+    body: "",
+    shield: ""
+  ],
+  aWeapon: [
+    {
+      name: "sword"
+    }
+  ],
+}
